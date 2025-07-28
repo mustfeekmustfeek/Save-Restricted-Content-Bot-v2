181205f90ce14fbbcaac82443a3b0f7cf86d0100
mustfeekmustfeek <masterearning9@gmail.com>
2025-07-28 11:17:01 +0530

    Update config.py

--- config.py
+++ config.py
@@ -13,13 +13,13 @@ YTUB_COOKIES = """
 # write here yt cookies
 """
 
-API_ID = int(getenv("5618662", "20310539"))
-API_HASH = getenv("bf2b762357b4b3c79bbb01e7529b81e2", "aec4988298a609673627f255a5adabc7")
-BOT_TOKEN = getenv("7398759794:AAGf2lx9-Z8OFJcZnGcuygY_cMLi_fCCTs0", "")
-OWNER_ID = list(map(int, getenv("7962530240", "6103594386").split()))
-MONGO_DB = getenv("mongodb+srv://mustfeek8:3VmnTgNJ5vdkSXC3@cluster0.evfwy2i.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", "")
-LOG_GROUP = getenv("-1004708871302", "")
-CHANNEL_ID = int(getenv("-1002885568913", "-1002511726148"))
+API_ID = int(getenv("API_ID", "5618662"))
+API_HASH = getenv("API_HASH", "bf2b762357b4b3c79bbb01e7529b81e2")
+BOT_TOKEN = getenv("BOT_TOKEN", "7398759794:AAGf2lx9-Z8OFJcZnGcuygY_cMLi_fCCTs0")
+OWNER_ID = list(map(int, getenv("OWNER_ID", "7962530240").split()))
+MONGO_DB = getenv("MONGO_DB", "mongodb+srv://mustfeek8:3VmnTgNJ5vdkSXC3@cluster0.evfwy2i.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+LOG_GROUP = getenv("LOG_GROUP", "-1004969139292")
+CHANNEL_ID = int(getenv("CHANNEL_ID", "-1002885568913"))
 FREEMIUM_LIMIT = int(getenv("FREEMIUM_LIMIT", "10"))
 PREMIUM_LIMIT = int(getenv("PREMIUM_LIMIT", "5000"))
 WEBSITE_URL = getenv("WEBSITE_URL", "upshrink.com")
